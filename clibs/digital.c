40f7e23c8679b7753e7a3b91ae0e92ad300d222a
Peter Elmegaard <elmegaard39@gmail.com>
2015-12-19 15:30:58 +0100

    Corrected native extern code functions

--- clibs/digital.c
+++ clibs/digital.c
@@ -1,9 +1,9 @@
 #include "types.h"
 #include "digitalWriteFast.h"
 
-extern int16 n_lib_Digital_Write(int32 *sp);
-extern int16 n_lib_Digital_PinMode(int32 *sp);
-extern int16 n_lib_Digital_read(int32 *sp);
+extern int16 n_HVMFramework_Digital_Write(int32 *sp);
+extern int16 n_HVMFramework_Digital_PinMode(int32 *sp);
+extern int16 n_HVMFramework_Digital_read(int32 *sp);
 
 int16 n_HVMFramework_Digital_Write(int32 *sp)
 {
